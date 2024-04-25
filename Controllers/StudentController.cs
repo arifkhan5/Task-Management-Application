1e120f05b2a4d6f683fbb82f790ac12562acd15f
arifkhan5 <ak7551907gmail.com>
2024-04-25 20:27:21 +0530

    task mangement application file uploading

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TaskManagementApplication.Controllers;
+using TaskManagementApplication.Data;
+using TaskManagementApplication.Models;
+using System.Threading.Tasks;
+using TaskManagementApplication.Models.Entities;
+
+namespace TaskManagementApplication.Controllers
+{
+
+     
+    public class StudentController : Controller
+    {
+        private readonly ApplicationDbContext dbContext;
+        public StudentController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddStudentViewModel viewModel) 
+        {
+
+            var student = new Student
+            {
+                Name = viewModel.Name,
+                Email = viewModel.Email,
+                Phone = viewModel.Phone,
+                Subscribed = viewModel.Subscribed
+              
+            };
+
+            await dbContext.Students.AddAsync(student);
+
+            await dbContext.SaveChangesAsync();
+
+            return View();
+        }
+
+        [HttpGet]
+
+        public async Task<IActionResult> List()
+        {
+            var students = await dbContext.Students.ToListAsync();
+            return View(students);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            var patient = await dbContext.Students.FindAsync(id);
+            return View(patient);
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Student viewModel)
+        {
+            var student = await dbContext.Students.FindAsync(viewModel.Id);
+
+            if (student is not null)
+            {
+                student.Name = viewModel.Name;
+                student.Email = viewModel.Email;
+                student.Phone = viewModel.Phone;
+                student.Subscribed = viewModel.Subscribed;
+
+                await dbContext.SaveChangesAsync();
+            }
+            return RedirectToAction("List", "Student");
+        }
+
+    }
+}
---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskManagementApplication.Models.Entities;
+
+namespace TaskManagementApplication.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        //This is a constructor
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Student> Students { get; set; }
+
+    }
+}
